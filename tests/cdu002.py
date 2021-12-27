102f6b775b3606a44a1fb0e866072f3fa2c76e7f
Arthur Medeiros <rthurmedeiros@gmail.com>
2021-12-26 22:35:49 -0300

    Finished cdu002 tests

--- tests/cdu002.py
+++ tests/cdu002.py
@@ -2,6 +2,7 @@ import unittest
 from browser import create_browser, login, sleep, APP_URL
 from util import random_word
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class CdU002TestCase(unittest.TestCase):
@@ -12,9 +13,10 @@ class CdU002TestCase(unittest.TestCase):
         sleep(2)
 
     def test_should_create_update_and_delete_project(self):
-        project_name = random_word()
-
         self.browser.get(APP_URL)
+
+        # CREATE
+        project_name = random_word()
 
         create_project_button = self.browser.find_element(By.ID, "menu-create-project")
         create_project_button.click()
@@ -25,15 +27,44 @@ class CdU002TestCase(unittest.TestCase):
 
         input_name.send_keys(project_name)
         input_submit.click()
-        sleep(4)
+        sleep(3)
 
-        created_project_card_label = self.browser.find_element(By.XPATH,
-                                                               "//span[@class='avatar-card-label font-weight-bold']")
+        project_card_label = self.browser.find_element(By.XPATH,
+                                                       "//span[@class='avatar-card-label font-weight-bold']")
 
-        self.assertEqual(created_project_card_label.text, project_name)
+        self.assertEqual(project_card_label.text, project_name)
 
-        # TODO: Update
-        # TODO: Delete
+        # UPDATE
+        project_name_2 = random_word()
+        project_card_label.click()
+        sleep(1)
+
+        input_name = self.browser.find_element(By.ID, "update-project-name")
+        input_submit = self.browser.find_element(By.ID, "update-project-submit")
+
+        input_name.send_keys(Keys.CONTROL + "a")
+        input_name.send_keys(Keys.DELETE)  # Erase old name
+        input_name.send_keys(project_name_2)
+        input_submit.click()
+        sleep(3)
+
+        project_card_label = self.browser.find_element(By.XPATH,
+                                                       "//span[@class='avatar-card-label font-weight-bold']")
+
+        self.assertEqual(project_card_label.text, project_name_2)
+        self.assertNotEqual(project_card_label.text, project_name)
+
+        # DELETE
+        project_card_label.click()
+        sleep(1)
+
+        delete_action = self.browser.find_element(By.ID, "update-project-remove")
+        delete_action.click()
+        sleep(3)
+
+        labels = self.browser.find_elements(By.XPATH, "//span[@class='avatar-card-label font-weight-bold']")
+
+        self.assertEqual(len(labels), 0)
 
 
 if __name__ == '__main__':
